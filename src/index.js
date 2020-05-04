5a4823f56f45d51199eb11f9864a27965ab92e9e
rozek1997 <marek.roskowicz@student.wat.edu.pl>
2020-05-04 21:11:16 +0200

    setup webpack and bable

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import "./index.css"
+
+const fun = () => {
+    return 2;
+}
+
+fun();
---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const path = require("path");
+const MiniExtractCssPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const VENDOR_LIBS = [
+    "lodash"
+]
+
+const config = {
+    mode: "development",
+    entry: {
+        bundle: "./src/index.js",
+        vendor: VENDOR_LIBS
+    },
+    output: {
+        path: path.join(__dirname, "build"),
+        filename: "[name].[chunkhash].js"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                options: {
+                    presets: ['@babel/preset-env']
+                }
+            },
+            {
+                test: /\.css$/i,
+                use: [MiniExtractCssPlugin.loader, "css-loader"],
+            }
+        ]
+    },
+    plugins: [
+        new MiniExtractCssPlugin({
+            filename: '[name].[hash].css',
+            chunkFilename: '[id].[hash].css',
+        }),
+        new HtmlWebpackPlugin({
+            template: "src/index.html"
+        }),
+    ],
+    optimization: {}
+
+}
+
+module.exports = config;
